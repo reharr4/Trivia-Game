3787800e5825da3549069ed4de67bbe3b214381d
Harrison, Rachel <reharr4@gmail.com>
2019-05-30 18:07:13 -0500

    start button and timer

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#start-button").click(function(){
+  // ----------------------------------------------------------------
+   // declaring the value for the timer to 60 seconds
+   // hide the start button and rules
+        var number = 60;
+      $("#start-button").on("click", start);  // starts the games 
+      
