db03031b978ad1dbc1d56b00d3d1645f82aaabe1
Harrison, Rachel <harrison.e.rachel@gmail.com>
2019-08-25 21:47:30 -0500

    timer working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,55 +1,112 @@
-$(document).ready(function(){
-    $("#start-button").click(function(){
-  // ----------------------------------------------------------------
-   // declaring the value for the timer to 60 seconds
-   // hide the start button and rules
-        var number = 60;
-      $("#start-button").on("click", start);  // starts the games 
-      $("#submit").on("click", finish);  // submits answers and finishes the game
-      $("#restart").on("click", restart);  // restarts the games 
+var card = $("#quiz");
 
-      function start(){
-    	counter = setInterval(timer, 1000);
-    	showMe(".question");
-    	showMe(".answers");
-    	showMe("#submit");
-	    hideMe("#start-button");
-	    hideMe("#restart");
-	    hideMe("#results");
+// questions
+var questions = [
+  {
+    question: "Whale Rider",
+    answers: ["Indie Band", "Foreign Film", "D&D Monster"],
+    correctAnswer: ""
+  },
+  {
+    question: "Sparklehorse",
+    answers: ["Indie Band", "Foreign Film", "D&D Monster"],
+    correctAnswer: ""
+  },
+  {
+    question: "Ooze Mephit",
+    answers: ["Indie Band", "Foreign Film", "D&D Monster"],
+    correctAnswer: ""
+  },
+  {
+    question: "Shambling Mound",
+    answers: ["Indie Band", "Foreign Film", "D&D Monster"],
+    correctAnswer: ""
+  },
+  {
+    question: "Imagine Dragons",
+    answers: ["Indie Band", "Foreign Film", "D&D Monster"],
+    correctAnswer: ""
+  },
+  {
+    question: "The Spirit of the Beehive",
+    answers: ["Indie Band", "Foreign Film", "D&D Monster"],
+    correctAnswer: ""
+  },
+  {
+    question: "Black Pudding",
+    answers: ["Indie Band", "Foreign Film", "D&D Monster"],
+    correctAnswer: ""
+  },
+  {
+    question: "Brain in a Jar",
+    answers: ["Indie Band", "Foreign Film", "D&D Monster"],
+    correctAnswer: ""
+  }
+];
+// declaring the value for the timer to 60 seconds
+var timer;
+
+var game = {
+  correct: 0,
+  incorrect: 0,
+  counter: 120,
+
+  countdown: function () {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME'S UP");
+      game.done();
     }
-    function timer(){
-      number-- // decrements the timer by 1
-      $("#show-time").html("<h2>" + number + "</h2>" );
-      if (number === 0){
-        stop(); // calls the stop function
+  },
+
+  start: function () {
+    timer = setInterval(game.countdown, 1000);
+
+    $("#sub-wrapper").prepend(
+      "<h2>Time Remaining: <span id='counter-number'>120</span> Seconds</h2>"
+    );
+
+    $("#start").remove();
+
+    for (var i = 0; i < questions.length; i++) {
+      card.append("<h2>" + questons[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        card.append("<input type='radio' name=question-" + i + "'value='" + questons[i].answer[j] + "''>" + question[i].answers[j]);
       }
     }
-    function stop(){
-    	clearInterval(counter); // stops the timer
-    	$("#results").show();
-    	$("#restart").show();
-		$(".question").hide();
-		$(".answers").hide();
-		$("#submit").hide();
-    }
-    function finish(){
-    	number = 1; // if number is equal to 0 number will show -1 so 1 has to be selected
-    	clearInterval(counter); // stops the timer
-    	timer();
-    }
 
-    function restart(){
-    	number = 60;
-    	start();
+    card.append("<button id='done'>Done</button>");
+  },
+
+  done: function () {
+    var inputs = card.children("input: checked");
+    for (var i = 0; i < inputs.length; i++) {
+      if ($(inputs[i]).val() === questions[i].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
     }
-    function hideMe(e) {
-    	$(e).hide();
-    }
-    function showMe(e) {
-    	$(e).show();
-    }
-    //calling functions
-  	start(); // calls the start function
+    this.result();
+  },
+
+  result: function () {
+    clearInterval(timer);
+
+    $("#sub-wrapper h2").remove();
+
+    card.html("<h2>All Done!</h2>");
+    card.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    card.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+  }
+};
+
+//calling functions
+$(document).on("click", "#start", function () {
+  game.start();
 });
+$(document).on("click", "#done", function () {
+  game.done();
 });
 
